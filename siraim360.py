1768fef41cca32d8cb9ce19e8d30e278c7e2faf7
siraim360 <81628515+siraim360@users.noreply.github.com>
2021-04-01 14:04:26 +0200

    Add files via upload

---
+++ siraim360.py
@@ -0,0 +1,38 @@
+name = "abba "
+department = "computer science"
+print(name + department)
+
+
+scare = "boo!"
+print(scare)
+
+
+a = 2
+b = 3
+c = a + b
+print ('\n',a,'\n',b,'\n',c,'\n')
+
+
+a = 12
+b = 0.5
+c = (a/b)
+print (c)
+
+
+a = 4
+b = 4
+c = (a+b)
+print (c)
+
+
+a = 8
+b = 3
+C = (8%3)
+print (C)
+
+a = 2
+b = 4
+c = a*b
+d = b+a
+result = c*d
+print (result)
